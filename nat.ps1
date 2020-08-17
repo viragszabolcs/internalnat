84e0bbfc9b4eae71558877b291387d0ca4453931
viragszabolcs <63259890+viragszabolcs@users.noreply.github.com>
2020-08-17 21:08:29 +0200

    Update nat.ps1

--- nat.ps1
+++ nat.ps1
@@ -1,4 +1,4 @@
 New-VMSwitch -SwitchName "NAT" -SwitchType Internal
 $adapter = Get-NetAdapter -Name *NAT*
-New-NetIPAddress -IPAddress 10.0.0.1
+New-NetIPAddress -IPAddress 10.0.0.1 -PrefixLength 24 -InterfaceAlias $adapter.Name
 New-NetNAT -Name "NAT" -InternalIPInterfaceAddressPrefix 10.0.0.0/24
